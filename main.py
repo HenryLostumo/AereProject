ccfdf8d1c03ec781fe4d54b4a72914dca2536748
Henry <henrylostumo@gmail.com>
2024-11-08 15:11:24 -0600

    probably finished

--- main.py
+++ main.py
@@ -18,27 +18,39 @@ earth_diameter = 1.27e7 * ur.meter
 earth_velocity = 460 * (ur.meter / ur.second)
 
 vec_pos = np.array([earth_diameter.magnitude / 2.0, 0.0]) * ur.meter
-vec_vel = np.array([earth_velocity.magnitude, 0]) * (ur.meter / ur.second)
+vec_vel = np.array([earth_velocity.magnitude, 0.0]) * (ur.meter / ur.second)
 thrust_angle = 30.0 * ur.degree  # counter clock wise from horizontal
 dir_thrust = np.array([np.cos(np.radians(thrust_angle)), np.sin(np.radians(thrust_angle))])
+
+pl.ion()
+fig = pl.figure()
+
+pl.axis((5e6, 8e6, -2e6, 2e6))
+pl.grid()
+
+earth = pl.Circle((0, 0), float(earth_diameter / 2.0 / ur.meter), color='g')
+fig.gca().add_artist(earth)
+
+arrowplt = pl.arrow(vec_pos.magnitude[0], vec_pos.magnitude[1],
+                    ((dir_thrust[0]) * 500000), ((dir_thrust[1]) * 500000), width=300000)
 
 t = 0 * ur.s
 
 
 def event_handler(event):
-    global dir_thrust
+    global thrust_angle
     if event.key == ',':
-        dir_thrust += 10 * ur.degree
+        thrust_angle += 10.0 * ur.degree
         pass
     elif event.key == '.':
-        dir_thrust -= 10 * ur.degree
+        thrust_angle -= 10.0 * ur.degree
         pass
     elif hasattr(event, "button") and event.button == 1:
-        if event.xdata < 0:
-            dir_thrust += 10 * ur.degree
+        if event.xdata < 0.0:
+            thrust_angle += 10 * ur.degree
             pass
         else:
-            dir_thrust -= 10 * ur.degree
+            thrust_angle -= 10.0 * ur.degree
             pass
         pass
 
@@ -46,7 +58,6 @@ def event_handler(event):
 
 
 def norm(vec):
-    vecs_unit = vec.units
     return np.sqrt(vec.dot(vec))  # * ur.vecs_unit
 
 
@@ -58,57 +69,48 @@ def unit_vec(vec):
     return vec / norm(vec)
 
 
-pl.ion()
-fig = pl.figure()
-ax = fig.gca()
-
-pl.axis((6e6, 7.5e6, -1e6, 1e6))
-pl.grid()
-
-earth = pl.Circle((0, 0), float(earth_diameter / 2.0 / ur.meter), color='g')
-ax.add_artist(earth)
-
-arrow_length = 200000
-
-arrowplt = pl.arrow(vec_pos.magnitude[0], vec_pos.magnitude[1],
-                    dir_thrust[0] * 500000, dir_thrust[1] * 500000)
-
 fig.canvas.mpl_connect('key_press_event', event_handler)
-
+fig.canvas.mpl_connect('button_press_event', event_handler)
+arrow_length = 350000
 pl.show()
 
 dt = 1 * ur.second
 
+current_mass = initial_mass
+
 while t <= 3600 * ur.second:
     t += dt
 
+    dir_thrust = np.array([np.cos(np.radians(thrust_angle)), np.sin(np.radians(thrust_angle))])
+
     if fuel_mass.magnitude > 0:
-        pl.axis((5.5e6, 8e6, -1e6, 1e6))
+        pl.axis((5e6, 8e6, -2e6, 2e6))
         dt = 1 * ur.second
         change_fuel_mass = ((-tsfc * full_thrust) * dt).to(ur.kg)
         fuel_mass += change_fuel_mass
-        current_mass = initial_mass + change_fuel_mass
+        current_mass += change_fuel_mass
     elif fuel_mass < 0 * ur.kg:
         print("Out of Fuel")
         pl.axis((-1e7, 1e7, -1e7, 1e7))
         fuel_mass = 0 * ur.kg
         dt = 10 * ur.second
-        current_mass = initial_mass
+        current_mass = initial_mass - 500000 * ur.kg
     else:
+        dt = 10 * ur.second
         pl.axis((-1e7, 1e7, -1e7, 1e7))
-        current_mass = initial_mass
-
-    vec_gravity = unit_vec(vec_pos) * force_gravity(norm(vec_pos), earth_mass, current_mass)
+        current_mass = initial_mass - 500000 * ur.kg
 
     if fuel_mass > 0:
-        vec_thrust = ((np.sqrt(dir_thrust.dot(dir_thrust))) / dir_thrust) * full_thrust
+        vec_thrust = dir_thrust * full_thrust
     else:
         vec_thrust = 0
 
+    vec_gravity = unit_vec(vec_pos) * force_gravity((norm(vec_pos)), earth_mass, current_mass)
+
     vec_accel = (vec_gravity + vec_thrust) / current_mass
 
-    vec_vel = vec_vel + vec_accel * dt  # Cannot do += here?
-    vec_pos += vec_vel * dt
+    vec_vel = vec_vel + (vec_accel * dt)  # Cannot do += here?
+    vec_pos = vec_pos + (vec_vel * dt)
 
     arrowplt.remove()
 
@@ -120,5 +122,5 @@ while t <= 3600 * ur.second:
     fig.canvas.draw()
     fig.canvas.flush_events()
 
-    pl.title(f'Fuel Remaining {np.round((fuel_mass.magnitude / 500000) * 100, 2)}%')
+    pl.title(f'Fuel Remaining {fuel_mass.magnitude} kg')
     pl.xlabel(f'Current Time: {t}s')
